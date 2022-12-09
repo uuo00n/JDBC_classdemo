1714b9aa97183d6aeae2d40bd147fe4b596fad67
uuo00_n <uuo00_n@outlook.com>
2022-12-09 19:42:31 +0800

    修改命名
    Signed-off-by: uuo00_n <uuo00_n@outlook.com>

--- web/sql/score&student.sql
+++ web/sql/score&student.sql
@@ -1,17 +1,15 @@
-create table if not exists score
+create table score
 (
     id     int         not null comment '编号'
-        constraint `PRIMARY`
         primary key,
     Stu_id int         not null comment '学号',
     C_name varchar(20) null comment '课程表',
     Grande int         null comment '分数'
 );
 
-create table if not exists student
+create table student
 (
     id         int         not null comment '学号'
-        constraint `PRIMARY`
         primary key,
     name       varchar(20) not null comment '姓名',
     sex        varchar(4)  null comment '性别',
